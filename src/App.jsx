b09d4a0b48f7d044e5d989e71b76dffd91306848
kalai02484 <kalai02484@gmail.com>
2026-03-05 01:31:07 +0530

    Router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Navbar from "./components/Navbar";
+import Blog from "./pages/Blog";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import ForgetPassword from "./pages/ForgetPassword";
+import ResetPassword from "./pages/ResetPassword";
+import CreateBlog from "./pages/CreateBlog";
+import Error from "./pages/Error";
+import AdminPanel from "./pages/AdminPanel";
+import Footer from "./components/Footer";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
     <div>
-      App Component
+      <div>
+        <ToastContainer />
+      </div>
+      <BrowserRouter>
+        <div>
+          <Navbar />
+        </div>
+        <Routes>
+          <Route path="/" element={<Blog />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forget-password" element={<ForgetPassword />} />
+          <Route
+            path="/reset-password/:id/:token"
+            element={<ResetPassword />}
+          />
+          <Route
+            path="/create-blog"
+            element={
+              <ProtectedRoute>
+                <CreateBlog />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin-panel"
+            element={
+              <ProtectedRoute adminOnly>
+                <AdminPanel />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <div>
+          <Footer />
+        </div>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
