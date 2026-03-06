6bb505f3a1188af65f91ff20a1e1e0d2c0c10987
kalai02484 <kalai02484@gmail.com>
2026-03-07 00:00:44 +0530

    Error Page Updated

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div>
-      
+    <div className="w-full h-[100vh] bg-amber-300 flex flex-col justify-center items-center"> 
+      <h1 className="text-6xl text-white font-serif font-bold mb-4">404 Not Found</h1>     
+      <p className="text-xl text-white mb-6">The page you are looking for does not exist.</p>
+      <Link to="/" className="text-xl text-white border-2 border-white px-4 py-2 ">Go to Home</Link>
     </div>
-  )
-}
+  );
+};
 
-export default Error
+export default Error;
